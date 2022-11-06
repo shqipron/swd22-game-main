15a0172d29069af974a87ee908cdd48b8c1ee682
Endrit <endritt.binaku@gmail.com>
2022-11-06 20:12:48 +0100

    added new modified map, updated TilesPath Class

--- core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/TilesPath.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/TilesPath.java
@@ -6,7 +6,7 @@ public class TilesPath {
     public static final String WALL = "tile_wall.png";
     public static final String BUSH = "bush.png";
     public static final String LAVA = "tile_lava.png";
-    public static final String PLAYER = "digimon.png";
+    public static final String PLAYER = "player.png";
     public static final String ENEMY_1 = "enemy_1.png";
     public static final String ENEMY_2 = "enemy_2.png";
     public static final String MAP = "map.png";
