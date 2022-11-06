a85e682480172408a80d1e316396a07a11b8a16d
shqipron <shqipronpopaj@gmail.com>
2022-11-06 23:17:42 +0100

    final commit for ue3

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/gameobjects/MapColors.java
@@ -0,0 +1,12 @@
+package at.compus02.swd.ss2022.game.gameobjects;
+
+public class TilesColors {
+    public static final int Water = 0x099cffff; // blue
+    public static final int Gras = 0x097b19ff; // green
+    public static final int Bush = 0x0fff26ff; //light green
+    public static final int Wall = 0x464646ff; //brown
+    public static final int Lava = 0x661b0aff; //dark red
+    public static final int Player = 0x000000ff; // black
+    public static final int Enemy1 = 0xed1c24ff; //red
+    public static final int Enemy2 = 0xfff200ff; //yellow
+}
